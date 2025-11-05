218f1942e8f9887ab45c76a6553784f7c87f1b82
Kiril Bogza <sobacika1999@gmail.com>
2025-11-05 20:34:57 +0100

    Added small details

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -7,7 +7,7 @@ type LayoutProps = {
 
 function Layout({ connectionStatus, messages }: LayoutProps) {
   const [isSideBarOpen, setSideBarOpen] = useState(false);
-  const [buttonUnactive, setButtonActive] = useState("");
+  const [buttonUnactive, setButtonActive] = useState(false);
 
   function openSidebar() {
     setSideBarOpen((prev) => !prev);
@@ -16,26 +16,46 @@ function Layout({ connectionStatus, messages }: LayoutProps) {
 
   return (
     <>
-      <header>
+      <header></header>
+      <main className="app">
         <div
           className="sidebar"
           style={{
             width: isSideBarOpen ? "300px" : "130px",
-            justifyContent: isSideBarOpen ? "flex-end" : "center",
+            justifyContent: isSideBarOpen ? "space-between" : "center",
+            flexDirection: isSideBarOpen ? "row-reverse" : "row-reverse",
           }}
         >
           {/* <p>{connectionStatus}</p> */}
 
-          <button onClick={openSidebar}>
+          <button onClick={openSidebar} className="button-sidebar">
             {buttonUnactive ? "→ ←" : "← →"}
           </button>
+          <div
+            style={{
+              display: isSideBarOpen ? "block" : "none",
+            }}
+          >
+            <div className="container-chats">
+              <p>chats</p>
+              <p>________</p>
+            </div>
+          </div>
         </div>
-      </header>
-      <main className="app">
-        <div className="message">
-          {messages.map((message) => (
-            <p>{message}</p>
-          ))}
+        <div className="container-chat">
+          <div className="message">
+            {messages.map((message) => (
+              <p>{message}</p>
+            ))}
+          </div>
+          <div className="container-input">
+            <input type="text" placeholder="Write your message" />
+            <button
+              className="button-submit"
+              type="submit"
+              title="none"
+            ></button>
+          </div>
         </div>
       </main>
 
